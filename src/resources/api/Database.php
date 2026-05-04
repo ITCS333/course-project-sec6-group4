dad436f002741ce2847df266cfb78360ef5950a9
zainab1234566778 <202202893@stu.uob.edu.bh>
2026-05-04 14:48:32 +0300

    Update Database.php

--- src/resources/api/Database.php
+++ src/resources/api/Database.php
@@ -1,24 +1,11 @@
 <?php
+require_once __DIR__ . '/db.php';
+
 class Database {
-    private $host = "127.0.0.1";
-    private $port = "3306";
-    private $db_name = "course";
-    private $username = "root";
-    private $password = "";
     public $conn;
 
     public function getConnection() {
-        $this->conn = null;
-        try {
-            $this->conn = new PDO(
-                "mysql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db_name . ";charset=utf8mb4",
-                $this->username,
-                $this->password
-            );
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch(PDOException $exception) {
-            echo "Connection error: " . $exception->getMessage();
-        }
+        $this->conn = getDBConnection();
         return $this->conn;
     }
 }
